ea1596226c38924a4d418113876ccd5f1d2e9e79
zy <zhaomin1423@foxmail.com>
2018-11-11 18:21:44 +0800

    add TokenFilter

---
+++ src/main/java/com/zy/apigateway/filter/AddResponseHeaderFilter.java
@@ -0,0 +1,26 @@
+package com.zy.apigateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+
+public class AddResponseHeaderFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return null;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return false;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        return null;
+    }
+}
--- src/main/java/com/zy/apigateway/filter/TokenFilter.java
+++ src/main/java/com/zy/apigateway/filter/TokenFilter.java
@@ -1,26 +1,58 @@
 package com.zy.apigateway.filter;
 
 import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_DECORATION_FILTER_ORDER;
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+
+/**
+ * 添加过滤器的步骤
+ * 1、@Component，让spring去管理
+ * 2、继承ZuulFilter
+ * 3、实现其方法
+ */
+@Component
 public class TokenFilter extends ZuulFilter {
     @Override
     public String filterType() {
-        return null;
+        return PRE_TYPE;
     }
 
     @Override
     public int filterOrder() {
-        return 0;
+        return PRE_DECORATION_FILTER_ORDER - 1;
     }
 
     @Override
     public boolean shouldFilter() {
-        return false;
+        return true;
     }
 
+    /**
+     * 过滤器的实现逻辑
+     * @return
+     * @throws ZuulException
+     */
     @Override
     public Object run() throws ZuulException {
+        //获取当前请求的上下文，可以获取request
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+        //从参数或cookie、header获取token
+        String token = request.getParameter("token");
+        if (StringUtils.isEmpty(token)){
+            requestContext.setSendZuulResponse(false);
+            requestContext.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
+        }
+        //处理逻辑
+        //通过ZuulResponse响应请求和状态码
         return null;
     }
 }
